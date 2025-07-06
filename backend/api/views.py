3e0880a3e6bb669ee45f3663eb2b14a8de6ae35a
Namitha Joseph <namithajosf.talrop@gmail.com>
2025-07-06 10:42:21 +0530

    feat: add view to fetch parishes and user details

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from .models import Parish
+from .serializers import ParishSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def parish_list(request):
+    parishes = Parish.objects.all()
+    serializer = ParishSerializer(parishes, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user_details(request):
+    user = request.user
+    serializer = UserSerializer(user)
+    return Response(serializer.data)
